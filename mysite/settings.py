d696ffae6028fbc9aa5b4faf097270bb36b58d14
ramonfrombr <ramonfrombr>
2023-05-07 19:39:03 -0300

    python-dotenv added to poetry. python version changed to 3.8

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
